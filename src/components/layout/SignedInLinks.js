c7fba15734e7d42cd50ae97019aec968013cb285
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-12-14 21:11:42 -0500

    all react

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -7,7 +7,7 @@ const SignedInLinks=()=>{
 	return (
      <div>
 		<ul className="right">
-		 <li> <NavLink to='/'>New Project </NavLink></li>
+		 <li> <NavLink to='/createproject'>New Project </NavLink></li>
 		 <li> <NavLink to='/'> Log Out</NavLink></li>
          <li> <NavLink to='/' className='btn btn-floating pink lighten-1'> LL </NavLink></li>
 		</ul> 
